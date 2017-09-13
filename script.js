6585c3ab7256b4d77c7eb6f1da0148b2c7cb30d3
JoseMejiaVelez <jose.mejia.velez@ucsp.edu.pe>
2017-09-13 16:59:32 -0500

    mi calculadora

---
+++ script.js
@@ -0,0 +1,106 @@
+//validar numeros
+
+function solonumeros(e){
+
+	key=e.keyCode || e.which;
+	teclado=String.fromCharCode(key);
+
+	numeros="0123456789";
+	especiales="8-37-39-46";
+	tecla_especial=false;
+
+	for(var i in especiales){
+
+		if (key==especiales[i]){
+
+			tecla_especial=true;
+
+		}
+	}
+	if(numeros.indexOf(teclado)==-1 && !tecla_especial){
+		return false;		
+	}
+
+}
+//para asignar valor
+
+function retornar(num){
+
+	var anterior=document.form.valores.value;
+	document.getElementById("valores").value=anterior+num;
+
+
+}
+
+//Asignar ultimo valor
+function eliminar(){
+
+	var anterior=document.form.valores.value;
+	var nuevovalor=anterior.substring(0,anterior.length-1)
+   document.getElementById("valores").value=nuevovalor;
+}
+
+//Eliminar todo
+function eliminar_todo(){
+
+	document.form.valores.value="";
+
+}
+//Para signos
+	function comprobar(num){ 
+	//var anterior = document.fo.valores.value;
+	var anterior = document.form.valores.value;
+	if(anterior==""){
+	document.form.valores.value="";
+	}else{    
+	    var anterior = document.form.valores.value;
+	    document.getElementById("valores").value=anterior+num;
+	    esto=document.form.valores.value;
+
+	    record=0; 
+	    igual=1; 
+	    var letraRecord 
+	    var b=0 
+	    var letra="" 
+
+	    for (a=1;a<esto.length;a++){      
+	    if (esto.charAt(a)=="+" || esto.charAt(a)=="-" || esto.charAt(a)=="*" || esto.charAt(a)=="/" || esto.charAt(a)=="."){ 
+	    igual=igual+1; 
+	    letra=esto.charAt(a);
+	    }else{ 
+	         if(igual>record){record=igual;letraRecord=letra} 
+	            igual=1 
+	         } 
+	         b=a 
+	    }
+	    
+	    if(igual>record){
+	      record=igual;
+	      letraRecord=letra;
+	    } 
+
+	    if (record>2){
+	        var anterior = esto;
+	        var nuevovalor = anterior.substring(0, anterior.length-1);
+	        document.getElementById("valores").value=nuevovalor;
+	        record=0;b=0;igual=1;letra="";letraRecord="";
+	    }     
+	} 
+
+}
+//realizar las operaciones
+
+
+function calcular(){
+    var resultado=eval(document.form.valores.value);
+
+    if(resultado=="Infinity"){
+        document.form.valores.value="No se puede dividir entre cero";
+
+    }else{
+        document.form.valores.value=resultado;
+    }
+    
+}
+	
+
